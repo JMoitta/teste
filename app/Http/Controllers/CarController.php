e3d2f584538838a6a8967d88b0807cb48c1be76a
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-06-10 22:28:54 -0300

    Criado novo controller para Carro

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace FederalSt\Http\Controllers;
+
+use FederalSt\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $carros = Car::orderBy('id', 'asc')->get();
+        return view('carro.index',[
+            "carros" => $carros
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('carro.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $car = new Car();
+        
+        $car->create($request->all());
+        // echo '$Car->create($request->all());';
+        return redirect()->route('admin.car.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $car = Car::find($id);
+        return view('carro.edit',
+            ['car' => $car]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $idArray = ['id' => $id];
+        
+        Car::updateOrCreate($idArray, $request->all());
+        return redirect()->route('admin.car.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $car = Car::find($id);
+        $car->delete();
+        return redirect()->route('admin.car.index');
+    }
+}
